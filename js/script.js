69defe6b2eccf64df7f1de5d7020b0e6763dbefe
Arkadiusz Adamik <arekadamik@gmail.com>
2021-05-09 13:50:12 +0200

    Build game structure

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
+
+function printMessage(msg){
+	let div = document.createElement('div');
+	div.innerHTML = msg;
+	document.getElementById('messages').appendChild(div);
+}
+
+function clearMessages(){
+	document.getElementById('messages').innerHTML = '';
+}
 let randomNumber = Math.floor(Math.random() * 3 + 1);
 
 console.log('Wylosowana liczba to: ' + randomNumber);
@@ -10,9 +20,54 @@ if(randomNumber == 1){
 else if(randomNumber == 2){
 	computerMove = 'papier';
 }
-else(randomNumber == 3){
+else if(randomNumber == 3){
 	computerMove = 'nożyce';
 }
 
 printMessage('Mój ruch to: ' + computerMove);
 
+let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
+
+console.log('Gracz wpisał: ' + playerInput);
+
+let playerMove = 'nieznany ruch';
+
+if(playerInput == '1'){
+	playerMove = 'kamień';
+}
+else if(playerInput == '2'){
+	playerMove = 'papier';
+}
+else if(playerInput == '3'){
+	playerMove = 'nożyce';
+}
+
+printMessage('Twój ruch to: ' + playerMove);
+
+if( computerMove == 'kamień' && playerMove == 'papier'){
+	printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'nożyce'){
+	printMessage('Przegrywasz!');
+}
+else if( computerMove == 'kamień' && playerMove == 'kamień'){
+	printMessage('Remis! Spróbuj ponownie!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'nożyce'){
+	printMessage('Remis! Spróbuj ponownie!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'papier'){
+	printMessage('Przegrywasz!');
+}
+else if( computerMove == 'nożyce' && playerMove == 'kamień'){
+	printMessage('Ty wygrywasz!');
+}
+else if( computerMove == 'papier' && playerMove == 'papier'){
+	printMessage('Remis! Spróbuj ponownie!');
+}
+else if( computerMove == 'papier' && playerMove == 'kamień'){
+	printMessage('Przegrywasz!');
+}
+else if( computerMove == 'papier' && playerMove == 'nożyce'){
+	printMessage('Ty wygrywasz!');
+}
