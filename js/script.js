2a59c8147786a1d07c717f9af30b90d9bd64d8a8
Arkadiusz Adamik <arekadamik@gmail.com>
2021-05-09 13:01:20 +0200

    Set computer move

--- js/script.js
+++ js/script.js
@@ -1,12 +1,18 @@
-function printMessage(msg){
-	let div = document.createElement('div');
-	div.innerHTML = msg;
-	document.getElementById('messages').appendChild(div);
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+	computerMove = 'kamień';
+}
+else if(randomNumber == 2){
+	computerMove = 'papier';
+}
+else(randomNumber == 3){
+	computerMove = 'nożyce';
 }
 
-function clearMessages() {
-	document.getElementById('messages').innerHTML = '';
-}
-let computerMove = `kamień`;
-let playerMove = `papier`;
-printMessage('Zagrałem ' + computerMove + '! Jeśli Twój ruch to ' + playerMove + ', to wygrywasz!');
+printMessage('Mój ruch to: ' + computerMove);
+
