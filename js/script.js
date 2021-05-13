c34ff6ab52c1e8acb182641330014d91c0aa5d2c
Arkadiusz Adamik <arekadamik@gmail.com>
2021-05-13 21:11:12 +0200

    Fix bug

--- js/script.js
+++ js/script.js
@@ -76,4 +76,3 @@ document.getElementById('play-paper').addEventListener('click', function(){
 document.getElementById('play-scissors').addEventListener('click', function(){
 	playGame(3);
   });
-  clearMessages(playGame);
